57d68f40e1270eeda020460b0fa67cf143d804f1
Zeroit Dev <zeroitdevnet@gmail.com>
2019-04-04 12:31:27 +0000

    Added Copyright Information to the Files

--- PolygonInput/PolyEditorDialog.Designer.cs
+++ PolygonInput/PolyEditorDialog.Designer.cs
@@ -1,4 +1,34 @@
-﻿namespace Zeroit.Framework.PolygonCreator
+﻿// ***********************************************************************
+// Assembly         : Zeroit.Framework.PolygonCreator
+// Author           : ZEROIT
+// Created          : 11-22-2018
+//
+// Last Modified By : ZEROIT
+// Last Modified On : 12-20-2018
+// ***********************************************************************
+// <copyright file="PolyEditorDialog.Designer.cs" company="Zeroit Dev Technologies">
+//    This program is for creating a Polygon control with an Editor.
+//    Copyright ©  2017  Zeroit Dev Technologies
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//    You can contact me at zeroitdevnet@gmail.com or zeroitdev@outlook.com
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace Zeroit.Framework.PolygonCreator
 {
     partial class PolyEditorDialog
     {
